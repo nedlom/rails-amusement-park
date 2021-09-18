e3ca3cf82a221f7db7695842f8510d87b2ddac37
Nick <nelom3@gmail.com>
2021-09-18 14:20:53 -0700

    commit tree 0e101223a35c857e9e3ae64a799b3fdb8d37190c

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,35 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride 
+    if enough_tickets? && tall_enough?
+      update_user
+    elsif !enough_tickets? && tall_enough?
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif enough_tickets? && !tall_enough?
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    elsif !enough_tickets? && !tall_enough?
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    end
+  end
+  
+  def enough_tickets?
+    self.user.tickets > self.attraction.tickets.to_i   
+  end
+  
+  def tall_enough?
+    self.user.height > self.attraction.min_height
+  end
+
+  def update_user
+    self.user.update(
+      tickets: self.user.tickets - self.attraction.tickets.to_i,  
+      happiness: self.user.happiness + self.attraction.happiness_rating,
+      nausea: self.user.nausea + self.attraction.nausea_rating 
+    )
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :attractions
+  resources :rides
+  resources :users
+
 end
